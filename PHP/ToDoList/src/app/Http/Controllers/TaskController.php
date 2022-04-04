87daeacbcd3f7571f2e80156a2c9504fe9ca8072
nakasone-asahi <nakasone.a217@i.softbank.jp>
2022-04-04 21:00:45 +0900

    add

--- PHP/ToDoList/src/app/Http/Controllers/TaskController.php
+++ PHP/ToDoList/src/app/Http/Controllers/TaskController.php
@@ -2,16 +2,37 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Folder;
+use App\Models\Task; 
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index() {
+    public function index(int $id) {
         
-        $folders = Folder::all();
+     // すべてのフォルダを取得する
+    $folders = Folder::all();
 
-        return view('tasks/index', [
-            'folders' => $folders,
-        ]);
+    // 選ばれたフォルダを取得する
+    $current_folder = Folder::find($id);
+
+    // 選ばれたフォルダに紐づくタスクを取得する
+    $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+    return view('tasks/index', [
+        'folders' => $folders,
+        'current_folder_id' => $current_folder->id,
+        'tasks' => $tasks,
+    ]);
     }
+
+/**
+ * GET /folders/{id}/tasks/create
+ */
+public function showCreateForm(int $id)
+{
+    return view('tasks/create', [
+        'folder_id' => $id
+    ]);
+}
 }
